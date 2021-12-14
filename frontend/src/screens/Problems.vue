ae25853fdebd34d1071a3fbfe13d570ecc3453a8
igor <igor.barbier@student.howest.be>
2021-12-14 22:48:06 +0100

    delete comment

--- frontend/src/screens/Problems.vue
+++ frontend/src/screens/Problems.vue
@@ -17,8 +17,7 @@ export default defineComponent({
                 'category 2',
                 'category 3',
                 'category 4',
-
-            ]
+            ],
         }
     },
 
@@ -31,17 +30,9 @@ export default defineComponent({
             console.log(value)
         },
 
-        getData() {
-            console.log('Getting data')
-            this.loading = true;
-            setTimeout(() => {
-                this.problems = data;
-                this.loading = false;
-            }, 500);
-        },
         handleSelect(e: any) {
-            console.log(e);
-        }
+            console.log(e)
+        },
     },
 
     setup() {
@@ -72,69 +63,93 @@ export default defineComponent({
         ></SearchBar>
 
         <!-- Filter -->
-
-        //FRONTENDDEVREP
-        <div
-            class="
-                mt-4
-                h-8
-                bg-dark-accent
-                rounded-full
-                inline-flex
-                items-center
-                px-4
-            "
-        >
-            <p class="text-dark-400 font-semibold mr-4">Choose category</p>
-            <svg class="w-4 h-4" viewBox="0 0 14.243 9.243">
-                <g transform="translate(2.121 2.121)">
-                    <line
-                        x2="4.95"
-                        y2="5"
-                        fill="none"
-                        stroke="#fff"
-                        stroke-linecap="round"
-                        stroke-width="3"
-                    />
-                    <line
-                        x1="4.95"
-                        y2="5"
-                        transform="translate(5.05)"
-                        fill="none"
-                        stroke="#fff"
-                        stroke-linecap="round"
-                        stroke-width="3"
-                    />
-                </g>
-            </svg>
-
-        //FRONTENDDEV
         <div class="relative">
-            <div class="mt-4 h-8 w-3/5 bg-dark-accent rounded-2xl inline-flex peer items-center px-4 justify-around" tabindex="0">
+            <div
+                class="
+                    mt-4
+                    h-8
+                    w-3/5
+                    bg-dark-accent
+                    rounded-2xl
+                    inline-flex
+                    peer
+                    items-center
+                    px-4
+                    justify-around
+                "
+                tabindex="0"
+            >
                 <p class="text-dark-400 font-semibold">Choose category</p>
                 <svg class="w-4 h-4" viewBox="0 0 14.243 9.243">
                     <g transform="translate(2.121 2.121)">
-                        <line x2="4.95" y2="5" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
-                        <line x1="4.95" y2="5" transform="translate(5.05)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
+                        <line
+                            x2="4.95"
+                            y2="5"
+                            fill="none"
+                            stroke="#fff"
+                            stroke-linecap="round"
+                            stroke-width="3"
+                        />
+                        <line
+                            x1="4.95"
+                            y2="5"
+                            transform="translate(5.05)"
+                            fill="none"
+                            stroke="#fff"
+                            stroke-linecap="round"
+                            stroke-width="3"
+                        />
                     </g>
                 </svg>
             </div>
 
-            <div 
+            <div
                 class="
-                    bg-dark-400 w-3/5 rounded-2xl mt-2 transition-all duration-300
-                    peer-focus:max-h-48 max-h-0
-                    peer-focus:opacity-100 opacity-0
+                    bg-dark-400
+                    w-3/5
+                    rounded-2xl
+                    mt-2
+                    transition-all
+                    duration-300
+                    peer-focus:max-h-48
+                    max-h-0
+                    peer-focus:opacity-100
+                    opacity-0
                     absolute
                     overflow-hidden
-                "  
+                "
             >
-                <div v-for="(category, index) in categories" :key="index" class="w-full">
-                    <input @change="handleSelect($event)" class="peer" :value="category" type="radio" hidden name="category" :id="`item${index}`">
-                    <label :for="`item${index}`" class="block p-2 px-4 w-full text-dark-900 peer-checked:bg-dark-accent peer-checked:text-dark-400 transition-colors duration-200">{{ category }}</label>
+                <div
+                    v-for="(category, index) in categories"
+                    :key="index"
+                    class="w-full"
+                >
+                    <input
+                        @change="handleSelect($event)"
+                        class="peer"
+                        :value="category"
+                        type="radio"
+                        hidden
+                        name="category"
+                        :id="`item${index}`"
+                    />
+                    <label
+                        :for="`item${index}`"
+                        class="
+                            block
+                            p-2
+                            px-4
+                            w-full
+                            text-dark-900
+                            peer-checked:bg-dark-accent
+                            peer-checked:text-dark-400
+                            transition-colors
+                            duration-200
+                        "
+                        >{{ category }}</label
+                    >
                 </div>
             </div>
-
         </div>
 
         <StorageList :data="problems" :loading="loading" class="mt-8">
