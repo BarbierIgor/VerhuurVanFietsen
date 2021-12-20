4ac405667c9d5f9db3611de5cf8eb00dd206be57
igor <igor.barbier@student.howest.be>
2021-12-20 15:43:02 +0100

    added pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+    registerType: 'autoUpdate',
+    mode: 'development',
+    base: '/',
+    includeAssets: ['favicon.svg'],
+    manifest: {
+        name: 'Bike rental',
+        short_name: 'Bike rental',
+        icons: [
+            {
+                src: '/android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/android-chrome-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+    },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), VitePWA()],
+    plugins: [vue(), VitePWA(pwaOptions)],
 })
